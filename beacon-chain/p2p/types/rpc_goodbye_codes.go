b1bfa0f5125335ed0da4fff2ded1cabecc18ca03
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-01-29 16:20:39 +0400

    beacon-chain/p2p/types/rpc_goodbye_codes.go: add github issue ref

--- beacon-chain/p2p/types/rpc_goodbye_codes.go
+++ beacon-chain/p2p/types/rpc_goodbye_codes.go
@@ -13,7 +13,7 @@ const (
 	GoodbyeCodeWrongNetwork
 	GoodbyeCodeGenericError
 
-	// TODO(rgeraldes24)
+	// TODO(theQRL/qrysm/issues/62)
 	// Teku specific codes
 	GoodbyeCodeUnableToVerifyNetwork = RPCGoodbyeCode(128)
 
@@ -25,13 +25,13 @@ const (
 
 // GoodbyeCodeMessages defines a mapping between goodbye codes and string messages.
 var GoodbyeCodeMessages = map[RPCGoodbyeCode]string{
-	GoodbyeCodeClientShutdown: "client shutdown",
-	GoodbyeCodeWrongNetwork:   "irrelevant network",
-	GoodbyeCodeGenericError:   "fault/error",
-	// GoodbyeCodeUnableToVerifyNetwork: "unable to verify network",
-	// GoodbyeCodeTooManyPeers:          "client has too many peers",
-	// GoodbyeCodeBadScore:              "peer score too low",
-	// GoodbyeCodeBanned:                "client banned this node",
+	GoodbyeCodeClientShutdown:        "client shutdown",
+	GoodbyeCodeWrongNetwork:          "irrelevant network",
+	GoodbyeCodeGenericError:          "fault/error",
+	GoodbyeCodeUnableToVerifyNetwork: "unable to verify network",
+	GoodbyeCodeTooManyPeers:          "client has too many peers",
+	GoodbyeCodeBadScore:              "peer score too low",
+	GoodbyeCodeBanned:                "client banned this node",
 }
 
 // ErrToGoodbyeCode converts given error to RPC goodbye code.
