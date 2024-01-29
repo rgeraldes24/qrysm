378d88cf7834961dfe35bd08fe5cea752b793592
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-01-29 11:40:25 +0400

    validator/accounts/wallet/wallet.go: comment unused var: remove var

--- validator/accounts/wallet/wallet.go
+++ validator/accounts/wallet/wallet.go
@@ -29,9 +29,6 @@ const (
 	PasswordPromptText = "Wallet password"
 	// ConfirmPasswordPromptText for confirming a wallet password.
 	ConfirmPasswordPromptText = "Confirm password"
-	// DefaultWalletPasswordFile used to store a wallet password with appropriate permissions
-	// if a user signs up via the Qrysm web UI via RPC.
-	// DefaultWalletPasswordFile = "walletpassword.txt"
 	// CheckExistsErrMsg for when there is an error while checking for a wallet
 	CheckExistsErrMsg = "could not check if wallet exists"
 	// CheckValidityErrMsg for when there is an error while checking wallet validity
