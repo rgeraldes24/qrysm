0681f3a86c4e4b994a9bec7718c3adcd137bd544
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-02-25 20:58:53 +0000

    beacon-chain/core/blocks/attestation_regression_test.go: review file

--- beacon-chain/core/blocks/attestation_regression_test.go
+++ beacon-chain/core/blocks/attestation_regression_test.go
@@ -1,52 +1,19 @@
 package blocks_test
 
-// NOTE(rgeraldes24): artifacts are not compatible with our version
+// TODO(rgeraldes24): fix unit test
 /*
 import (
 	"context"
-	"os"
 	"testing"
 
 	"github.com/theQRL/go-bitfield"
 	"github.com/theQRL/qrysm/v4/beacon-chain/core/blocks"
-	state_native "github.com/theQRL/qrysm/v4/beacon-chain/state/state-native"
 	"github.com/theQRL/qrysm/v4/config/params"
 	zondpb "github.com/theQRL/qrysm/v4/proto/qrysm/v1alpha1"
 	"github.com/theQRL/qrysm/v4/testing/assert"
 	"github.com/theQRL/qrysm/v4/testing/require"
 	"github.com/theQRL/qrysm/v4/testing/util"
 )
-
-// Beaconfuzz discovered an off by one issue where an attestation could be produced which would pass
-// validation when att.Data.CommitteeIndex is 1 and the committee count per slot is also 1. The only
-// valid att.Data.Committee index would be 0, so this is an off by one error.
-// See: https://github.com/sigp/beacon-fuzz/issues/78
-func TestProcessAttestationNoVerifySignature_BeaconFuzzIssue78(t *testing.T) {
-	attData, err := os.ReadFile("testdata/beaconfuzz_78_attestation.ssz")
-	if err != nil {
-		t.Fatal(err)
-	}
-	att := &zondpb.Attestation{}
-	if err := att.UnmarshalSSZ(attData); err != nil {
-		t.Fatal(err)
-	}
-	stateData, err := os.ReadFile("testdata/beaconfuzz_78_beacon.ssz")
-	if err != nil {
-		t.Fatal(err)
-	}
-	spb := &zondpb.BeaconState{}
-	if err := spb.UnmarshalSSZ(stateData); err != nil {
-		t.Fatal(err)
-	}
-	st, err := state_native.InitializeFromProtoUnsafePhase0(spb)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ctx := context.Background()
-	_, err = blocks.ProcessAttestationNoVerifySignature(ctx, st, att)
-	require.ErrorContains(t, "committee index 1 >= committee count 1", err)
-}
 
 // Regression introduced in https://github.com/theQRL/qrysm/pull/8566.
 func TestVerifyAttestationNoVerifySignature_IncorrectSourceEpoch(t *testing.T) {
@@ -76,7 +43,7 @@ func TestVerifyAttestationNoVerifySignature_IncorrectSourceEpoch(t *testing.T) {
 	require.NoError(t, beaconState.SetCurrentJustifiedCheckpoint(ckp))
 	require.NoError(t, beaconState.AppendCurrentEpochAttestations(&zondpb.PendingAttestation{}))
 
-	err = blocks.VerifyAttestationNoVerifySignature(context.TODO(), beaconState, att)
+	err = blocks.VerifyAttestationNoVerifySignatures(context.TODO(), beaconState, att)
 	assert.NotEqual(t, nil, err)
 }
 */
