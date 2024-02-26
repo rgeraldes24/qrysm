998e784039df4269509fca9cdaef04f905463f9f
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-02-26 09:51:48 +0000

    beacon-chain/core/blocks/attester_slashing_test.go: review file

--- beacon-chain/core/blocks/attester_slashing_test.go
+++ beacon-chain/core/blocks/attester_slashing_test.go
@@ -158,8 +158,6 @@ func TestProcessAttesterSlashings_AppliesCorrectStatusCapella(t *testing.T) {
 		)
 	}
 
-	// TODO(rgeraldes24): double check
-	// require.Equal(t, uint64(31000000000), newState.Balances()[1])
 	require.Equal(t, uint64(38750000000000), newState.Balances()[1])
 	require.Equal(t, uint64(40000000000000), newState.Balances()[2])
 }
