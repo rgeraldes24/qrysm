3c7d496f02264d8bb714a6f7f51e077bade3acb9
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-02-26 09:06:17 +0000

    beacon-chain/blockchain/metrics_test.go: review file

--- beacon-chain/blockchain/metrics_test.go
+++ beacon-chain/blockchain/metrics_test.go
@@ -15,12 +15,13 @@ func TestReportEpochMetrics_BadHeadState(t *testing.T) {
 	require.NoError(t, err)
 	require.NoError(t, h.SetValidators(nil))
 	err = reportEpochMetrics(context.Background(), s, h)
-	// require.ErrorContains(t, "failed to initialize precompute: state has nil validator slice", err)
-	// NOTE(rgeraldes24) - initial message in the capella flow is different; double check
 	require.ErrorContains(t, "could not read every validator: state has nil validator slice", err)
 }
 
-// TODO(rgeraldes24): same as below, old flow, replace with new tests?
+// NOTE(rgeraldes24): These tests are no longer valid since they use the phase0 flow
+// that uses the AttestedCurrentEpoch func which throws an error. The Capella flow
+// includes the participation bits to identify the ones that attested and that func
+// (AttestedCurrentEpoch) is no longer necessary. Replace with new tests?
 /*
 func TestReportEpochMetrics_BadAttestation(t *testing.T) {
 	s, err := util.NewBeaconStateCapella()
@@ -32,13 +33,7 @@ func TestReportEpochMetrics_BadAttestation(t *testing.T) {
 	err = reportEpochMetrics(context.Background(), s, h)
 	require.ErrorContains(t, "attestation with inclusion delay of 0", err)
 }
-*/
 
-// NOTE(rgeraldes24): This test is no longer valid since it uses the phase0 flow
-// that uses the AttestedCurrentEpoch func which throws this error.
-// The Capella flow includes the participation bits to identify the ones that attested
-// and that func (AttestedCurrentEpoch) is no longer necessary.
-/*
 func TestReportEpochMetrics_SlashedValidatorOutOfBound(t *testing.T) {
 	h, _ := util.DeterministicGenesisStateCapella(t, 1)
 	v, err := h.ValidatorAtIndex(0)
