e44741531c197e8a616713909457a1dee04b6d2a
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-02-27 10:29:32 +0000

    beacon-chain/rpc/zond/helpers/sync.go: review file

--- beacon-chain/rpc/zond/helpers/sync.go
+++ beacon-chain/rpc/zond/helpers/sync.go
@@ -102,7 +102,7 @@ func IsOptimistic(
 				return false, err
 			}
 			return isStateRootOptimistic(ctx, id, optimisticModeFetcher, stateFetcher, chainInfo, database)
-			// TODO(rgeraldes24): review
+			// TODO(rgeraldes24): review 32
 		} else if len(stateId) == 32 {
 			return isStateRootOptimistic(ctx, stateId, optimisticModeFetcher, stateFetcher, chainInfo, database)
 		} else {
