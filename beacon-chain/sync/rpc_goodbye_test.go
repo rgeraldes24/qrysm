4eab3f5dfa56c629809ade17f677d12d29cc1d1f
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-03-14 18:36:14 +0400

    beacon-chain/p2p: review NewTestP2P func

--- beacon-chain/sync/rpc_goodbye_test.go
+++ beacon-chain/sync/rpc_goodbye_test.go
@@ -13,14 +13,14 @@ import (
 	p2ptest "github.com/theQRL/qrysm/v4/beacon-chain/p2p/testing"
 	p2ptypes "github.com/theQRL/qrysm/v4/beacon-chain/p2p/types"
 	"github.com/theQRL/qrysm/v4/beacon-chain/startup"
+	"github.com/theQRL/qrysm/v4/config/params"
 	"github.com/theQRL/qrysm/v4/consensus-types/primitives"
 	leakybucket "github.com/theQRL/qrysm/v4/container/leaky-bucket"
 	"github.com/theQRL/qrysm/v4/testing/assert"
+	"github.com/theQRL/qrysm/v4/testing/require"
 	"github.com/theQRL/qrysm/v4/testing/util"
 )
 
-// TODO(rgeraldes24): fix unit test: fails since moving to go1.21
-/*
 func TestGoodByeRPCHandler_Disconnects_With_Peer(t *testing.T) {
 	params.SetupTestConfigCleanup(t)
 	cfg := params.MainnetConfig().Copy()
@@ -145,7 +145,6 @@ func TestGoodByeRPCHandler_BackOffPeer(t *testing.T) {
 	// Add a little bit of allowance
 	require.Equal(t, true, diff.Seconds() <= 1)
 }
-*/
 
 func TestSendGoodbye_SendsMessage(t *testing.T) {
 	p1 := p2ptest.NewTestP2P(t)
