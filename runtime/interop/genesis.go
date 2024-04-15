0607903948992b6f0c0f0beb0f3ad79e5d3794bf
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-04-15 21:36:21 +0400

    Remove Istanbul fork part 2

--- runtime/interop/genesis.go
+++ runtime/interop/genesis.go
@@ -83,7 +83,6 @@ func GzondTestnetGenesis(genesisTime uint64, cfg *clparams.BeaconChainConfig) *c
 	shanghaiTime := GzondShanghaiTime(genesisTime)
 	cc := &params.ChainConfig{
 		ChainID:                       big.NewInt(defaultTestChainId),
-		IstanbulBlock:                 bigz,
 		MuirGlacierBlock:              bigz,
 		BerlinBlock:                   bigz,
 		LondonBlock:                   bigz,
