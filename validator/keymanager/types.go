89f0aabb35ec726862bdc3650b6828ed3f5b2536
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-07-02 15:55:07 +0400

    Review files

--- validator/keymanager/types.go
+++ validator/keymanager/types.go
@@ -106,7 +106,7 @@ const (
 
 // @NOTE(rgeraldes24):
 // QRL Version - https://github.com/theQRL/go-zond-wallet-encryptor-keystore/blob/main/decrypt.go#L87
-// Eth Version - https://github.com/wealdtech/go-eth2-wallet-encryptor-keystorev1/blob/master/decrypt.go#L168
+// Eth Version - https://github.com/wealdtech/go-eth2-wallet-encryptor-keystorev4/blob/master/decrypt.go#L168
 // IncorrectPasswordErrMsg defines a common error string representing an EIP-2335
 // keystore password was incorrect.
 // const IncorrectPasswordErrMsg = "invalid checksum"
