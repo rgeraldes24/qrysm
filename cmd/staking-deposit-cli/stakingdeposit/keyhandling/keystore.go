942d11306c204bffe1fdf29fc24eb0dcfcd57748
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-07-02 21:06:09 +0400

    cmd/staking-deposit-cli: review files

--- cmd/staking-deposit-cli/stakingdeposit/keyhandling/keystore.go
+++ cmd/staking-deposit-cli/stakingdeposit/keyhandling/keystore.go
@@ -1,30 +1,27 @@
 package keyhandling
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"runtime"
 
 	"github.com/google/uuid"
 	"github.com/theQRL/go-qrllib/dilithium"
+	"github.com/theQRL/go-zond/accounts/keystore"
 	"github.com/theQRL/qrysm/cmd/staking-deposit-cli/misc"
 	field_params "github.com/theQRL/qrysm/config/fieldparams"
-	"golang.org/x/crypto/sha3"
+	"github.com/theQRL/qrysm/encoding/bytesutil"
 )
 
 type Keystore struct {
-	Crypto      *KeystoreCrypto `json:"crypto"`
-	Description string          `json:"description"`
-	PubKey      string          `json:"pubkey"`
-	Path        string          `json:"path"`
-	UUID        string          `json:"uuid"`
-	Version     uint64          `json:"version"`
+	Crypto      keystore.CryptoJSON `json:"crypto"`
+	Description string              `json:"description"`
+	PubKey      string              `json:"pubkey"`
+	Path        string              `json:"path"`
+	UUID        string              `json:"uuid"`
+	Version     uint64              `json:"version"`
 }
 
 func (k *Keystore) ToJSON() []byte {
@@ -59,41 +56,12 @@ func (k *Keystore) Save(fileFolder string) error {
 	return nil
 }
 
-// TODO(rgeraldes24)
 func (k *Keystore) Decrypt(password string) [field_params.DilithiumSeedLength]byte {
-	salt, ok := k.Crypto.KDF.Params["salt"]
-	if !ok {
-		panic("salt not found in KDF Params")
-	}
-	binSalt := misc.DecodeHex(salt.(string))
-
-	decryptionKey, err := passwordToDecryptionKey(password, binSalt)
+	seed, err := keystore.DecryptDataV1(k.Crypto, password)
 	if err != nil {
-		panic(fmt.Errorf("passwordToDecryptionKey | reason %v", err))
+		panic(fmt.Errorf("keystore.DecryptDataV1 failed | reason %v", err))
 	}
-
-	block, err := aes.NewCipher(decryptionKey[:])
-	if err != nil {
-		panic(fmt.Errorf("aes.NewCipher failed | reason %v", err))
-	}
-
-	cipherText := misc.DecodeHex(k.Crypto.Cipher.Message)
-	aesIV, ok := k.Crypto.Cipher.Params["iv"]
-	if !ok {
-		panic(fmt.Errorf("aesIV not found in Cipher Params"))
-	}
-	binAESIV := misc.DecodeHex(aesIV.(string))
-
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err)
-	}
-	plainText, err := aesgcm.Open(nil, binAESIV, cipherText, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	return [field_params.DilithiumSeedLength]byte(plainText)
+	return bytesutil.ToBytes48(seed)
 }
 
 func NewKeystoreFromJSON(data []uint8) *Keystore {
@@ -113,41 +81,23 @@ func NewKeystoreFromFile(path string) *Keystore {
 	return NewKeystoreFromJSON(data)
 }
 
+func NewEmptyCryptoJSON() keystore.CryptoJSON {
+	return keystore.CryptoJSON{
+		KDFParams: map[string]interface{}{},
+	}
+}
+
 func NewEmptyKeystore() *Keystore {
 	k := &Keystore{}
-	k.Crypto = NewEmptyKeystoreCrypto()
+	k.Crypto = NewEmptyCryptoJSON()
 	return k
 }
 
-func Encrypt(seed [field_params.DilithiumSeedLength]uint8, password, path string, salt, aesIV []byte) (*Keystore, error) {
-	if salt == nil {
-		salt = make([]uint8, 32)
-		if _, err := io.ReadFull(rand.Reader, salt); err != nil {
-			return nil, err
-		}
-	}
-	if aesIV == nil {
-		aesIV = make([]uint8, 12)
-		if _, err := io.ReadFull(rand.Reader, aesIV); err != nil {
-			return nil, err
-		}
-	}
-
-	decryptionKey, err := passwordToDecryptionKey(password, salt)
+func Encrypt(seed [field_params.DilithiumSeedLength]uint8, password, path string) (*Keystore, error) {
+	cjson, err := keystore.EncryptDataV1(seed[:], []byte(password), keystore.StandardArgon2idT, keystore.StandardArgon2idM, keystore.StandardArgon2idP)
 	if err != nil {
 		return nil, err
 	}
-
-	block, err := aes.NewCipher(decryptionKey[:])
-	if err != nil {
-		return nil, err
-	}
-
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-	cipherText := aesgcm.Seal(nil, aesIV, seed[:], nil)
 
 	d, err := dilithium.NewDilithiumFromSeed(seed)
 	if err != nil {
@@ -156,24 +106,8 @@ func Encrypt(seed [field_params.DilithiumSeedLength]uint8, password, path string
 	pk := d.GetPK()
 	return &Keystore{
 		UUID:   uuid.New().String(),
-		Crypto: NewKeystoreCrypto(salt, aesIV, cipherText),
+		Crypto: cjson,
 		PubKey: misc.EncodeHex(pk[:]),
 		Path:   path,
 	}, nil
-}
-
-// TODO(rgeraldes24): remove
-func passwordToDecryptionKey(password string, salt []byte) ([32]byte, error) {
-	h := sha3.NewShake256()
-	if _, err := h.Write([]byte(password)); err != nil {
-		return [32]byte{}, fmt.Errorf("shake256 hash write failed %v", err)
-	}
-
-	if _, err := h.Write(salt); err != nil {
-		return [32]byte{}, fmt.Errorf("shake256 hash write failed %v", err)
-	}
-
-	var decryptionKey [32]uint8
-	_, err := h.Read(decryptionKey[:])
-	return decryptionKey, err
 }
--- cmd/staking-deposit-cli/stakingdeposit/keyhandling/keystoremodule.go
+++
@@ -1,7 +0,0 @@
-package keyhandling
-
-type KeystoreModule struct {
-	Function string                 `json:"function"`
-	Params   map[string]interface{} `json:"params"`
-	Message  string                 `json:"message"`
-}
