0235a975bb4929605598774f085ebe334e219827
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-08-05 14:06:48 +0400

    config/params/loader_test.go: review file

--- config/params/loader_test.go
+++ config/params/loader_test.go
@@ -152,7 +152,7 @@ func assertEqualConfigs(t *testing.T, name string, fields []string, expected, ac
 
 func TestModifiedE2E(t *testing.T) {
 	c := params.E2ETestConfig().Copy()
-	c.DepositContractAddress = "Q§4242424242424242424242424242424242424242"
+	c.DepositContractAddress = "Q4242424242424242424242424242424242424242"
 	y := params.ConfigToYaml(c)
 	cfg, err := params.UnmarshalConfig(y, nil)
 	require.NoError(t, err)
