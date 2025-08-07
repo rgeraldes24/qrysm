b60c8a39456a2c27aefab0fcc9d37bd9bb3da41b
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-08-07 15:40:09 +0400

    testing/endtoend/components/web3remotesigner.go: review files

--- testing/endtoend/components/web3remotesigner.go
+++ testing/endtoend/components/web3remotesigner.go
@@ -89,7 +89,7 @@ func (w *Web3RemoteSigner) Start(ctx context.Context) error {
 		fmt.Sprintf("--http-listen-port=%d", Web3RemoteSignerPort),
 		"--logging=ALL",
 		// Command
-		"beacon",
+		"consensus",
 		// Command flags
 		"--network=" + network,
 		"--slashing-protection-enabled=false", // Otherwise, a postgres DB is required.
