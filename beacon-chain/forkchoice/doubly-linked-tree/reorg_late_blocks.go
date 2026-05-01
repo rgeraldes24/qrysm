5e0cf7c97b43019766ea47449dabf4f950be6119
cyyber <kaushal.forex@gmail.com>
2026-05-01 14:46:53 +0400

    Fix: (upstream 13683) Do not check parent weight on early FCU

--- beacon-chain/forkchoice/doubly-linked-tree/reorg_late_blocks.go
+++ beacon-chain/forkchoice/doubly-linked-tree/reorg_late_blocks.go
@@ -83,6 +83,17 @@ func (f *ForkChoice) ShouldOverrideFCU() (override bool) {
 		return
 	}
 
+	// Return early if we are checking before attestations for the current slot
+	// have been processed. The parent LMD weight check below relies on those
+	// attestations and would otherwise veto legitimate reorgs of late blocks.
+	secs, err := slots.SecondsSinceSlotStart(head.slot, f.store.genesisTime, uint64(time.Now().Unix()))
+	if err != nil {
+		log.WithError(err).Error("could not check current slot")
+		return true
+	}
+	if secs < ProcessAttestationsThreshold {
+		return true
+	}
 	// Only orphan a block if the parent LMD vote is strong
 	if parent.weight*100 < f.store.committeeWeight*params.BeaconConfig().ReorgParentWeightThreshold {
 		return
